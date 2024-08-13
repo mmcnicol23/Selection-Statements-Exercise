9000ab7acb014aab46fb71dbbbd14edbb6afc539
Mariah McNicol <mariahmcnicol@gmail.com>
2024-08-13 14:26:07 -0400

    updated exercise 1 with the number guessing game with loop

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -3,8 +3,35 @@
     public class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 4;
+            bool i = true;
+            while (i == true)
+            {
+                Console.WriteLine("Guess my favorite number between 1 and 10!");
+                int userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < 7)
+                {
+                    Console.WriteLine("Too low!");
+                    i = true;
+                }
+
+                else if (userInput > 7)
+                {
+                    Console.WriteLine("Too high!");
+                    i = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("You guessed it! Great job!");
+                    i = false;
+                }
+            }
         }
     }
+    
 }
+    
